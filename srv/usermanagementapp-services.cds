9e864b45952300d5db345d566452953e69d829eb
Chan Chiew Voon <CChan@ppg.com>
2022-06-02 06:58:04 +0000

    added uaa,fixes on destination

--- srv/usermanagementapp-services.cds
+++ srv/usermanagementapp-services.cds
@@ -2,6 +2,7 @@ using usermanagementapp as uman from '../db/data-model';
 // using ZC_USERS_CDS as users_ext from './external/ZC_USERS_CDS.csn';
 
 service usermanagementapp_services {
+    @requires : 'authenticated-user'
     @Capabilities.Insertable : true
     @Capabilities.Updatable  : true
     @Capabilities.Deletable  : true
--- srv/usermanagementapp-services.js
+++ srv/usermanagementapp-services.js
@@ -1,5 +1,6 @@
 const { executeHttpRequest } = require('@sap-cloud-sdk/core');
 const cds = require('@sap/cds');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module.exports = (srv) => {
     srv.before("CREATE", "incidents", async (req) => {
@@ -15,42 +16,90 @@ module.exports = (srv) => {
         req.data.status = `PENDING`;
         req.data.approverid = `K009287`;
         console.log(`Request details updated`);
+        createwf(req);
+        
     })
 
-    srv.after('CREATE', 'incidents', async (req, msg) => {
-        console.log("Start of after create");
-        console.log(msg.data);
-        createwf(msg.data);
+    // srv.after('CREATE', 'incidents', async (each, msg) => {
+        // console.log("Start of after create");
+        // console.log(each);
+        // console.log("msg");
+        // console.log(msg.data);
+        // createwf(msg.data);
         // console.log("Start of AFTER hook");
         // sendmailrequest(msg.data);
-    })
+    // })
 
 }
 
-const createwf = async (incident) => {
+const createwf = async (req) => {
     console.log("Preparing WF Payload");
     const payload = {
         definitionId: "usermanagementapproval",
         context: {
             approvalrequest: {
-                ticketno: incident.ticket_no,
-                userid: incident.targetid_bname,
-                system: incident.system,
-                client: incident.client
+                ticketno: req.data.ticket_no,
+                userid: req.data.targetid,
+                system: req.data.system,
+                client: req.data.client,
+                approver: "cchan@ppg.com"
             }
         }
     };
 
     console.log("Sending Workflow Request");
-    const response = await executeHttpRequest(
-        {
-            destinationName: "bpmworkflowruntime"
-        },
-        {
-            method: 'POST',
-            data: payload,
-            url: "/rest/v1/workflow-instances"
-        }
-    );
-    console.log(response.data);
+    try {
+        const response = await executeHttpRequest(
+            {
+                destinationName: "bpmworkflowruntime_oacc"
+                // destinationName: "br_bpmworkflowruntime"
+            },
+            {
+                method: 'POST',
+                data: payload,
+                url: "/rest/v1/workflow-instances"
+            }
+        );
+        console.log(response.data);
+    }
+    catch(error){
+        req.info("Error during WF instance creation. Check with technical team for more details.")
+        console.log(error);
+    }
+};
+
+
+const sendmailrequest = async (incident) => {
+    console.log("Incident Ticket No:", incident.ticket_no);
+    const mailcontent = {
+        sender: 'SAPCOEBTPGeneral@ppg.com',
+        recipient: 'cchan@ppg.com',
+        subject: 'Approval request requiring your attention',
+        body: `Kindly login to your workflow inbox to approve or reject the User management ticket, ${incident.ticketno}`
+    };
+
+    const payjson = JSON.stringify(mailcontent);
+
+    try {
+        console.log("Preparing to send request");
+        // const response = await executeHttpRequest(
+        //     {
+        //         destinationName: "Mail_Service_Api"
+        //     },
+        //     {
+        //         method: 'POST',
+        //         data: payjson,
+        //         url: "/mailrequests"
+        //     }
+        // );
+        // console.log(response.status);
+    }
+
+    catch (error) {
+        // console.log("Error in sending request:", error.response.data.error.message)
+        console.log("Error!!!!", error);
+        return;
+    }
+    console.log("Mail sent successfully");
+
 };
